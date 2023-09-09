20e266f0a70455b067e330fdf6006ecf8cdd97c1
Uranium227 <104591063+Uranium227@users.noreply.github.com>
2023-09-09 17:05:42 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,56 @@
+"""
+Calculator for python 3
+
+
+"""
+
+
+import math
+
+while True:
+     try:
+          function = int(input(""""
+                           1 +
+                           2 -
+                           3 *
+                           4 /
+                           Choose one: 
+                           """))
+          if(function >= 5 or function<=0):
+               print("Write a number in 1 - 4")
+          else:
+               break
+     except ValueError:
+          print("Write a number")
+
+while True:          
+     try:
+          firstNumber = float(input("Write First Number"))
+          break
+     except ValueError:
+          print("Write a Number")
+          
+while True:
+     try:
+          secondNumber = float(input("Write Second Number"))
+          break
+     except ValueError:
+          print("Write a Number")
+          
+          
+if(function == 1):
+     print(firstNumber+secondNumber)
+     
+elif(function == 2):
+     print(firstNumber-secondNumber)
+
+elif(function == 3):
+     print(firstNumber*secondNumber)
+     
+elif(function == 4):
+     if(secondNumber != 0):
+          print(firstNumber/secondNumber)
+     else:
+          print("No number can be divided by 0")
+
+
